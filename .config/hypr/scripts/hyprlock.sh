1b0c1d46bb0a7eb6a1c5965e301b7dca0afef50d
x <bladeec1ty@protonmail.com>
2024-04-09 11:30:08 +0300

    update hyprlock.sh

--- .config/hypr/scripts/hyprlock.sh
+++ .config/hypr/scripts/hyprlock.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 lastwall=$(cat $HOME/.cache/swww/eDP-1)
-img=/tmp/hyprlock.png
+img=$HOME/.cache/hyprland/hyprlock.png
 
-prev_wall=$(cat /tmp/.hyprlock_lw)
+prev_wall=$(cat $HOME/.cache/hyprland/.hyprlock_lw)
 
-echo $lastwall > /tmp/.hyprlock_lw
+echo $lastwall > $HOME/.cache/hyprland/.hyprlock_lw
 
 if [[ $lastwall == $prev_wall ]]; then
     hyprlock
